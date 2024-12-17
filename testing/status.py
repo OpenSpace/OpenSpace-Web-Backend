fe5edcf2f987565fdee467d6581ca22910ddfdc6
GPayne <payne.gene@gmail.com>
2024-12-17 15:41:46 -0700

    Improved status test command

--- testing/status.py
+++ testing/status.py
@@ -1,13 +1,24 @@
 import json
+import sys
 import testSend as ts
 
-ts.sendMessage("{\"command\": \"SERVER_STATUS\"}")
-capacity = ts.getResult()
-jResult = json.loads(capacity)
-for i in range(jResult["total"], 0, -1):
-    ts.sendMessage("{\"command\": \"STATUS\", \"id\": " + str(i - 1) + "}")
+def getStatusForId(id):
+    ts.sendMessage("{\"command\": \"STATUS\", \"id\": " + str(id) + "}")
     rsp = json.loads(ts.getResult())
-    if rsp["status"] != "IDLE":
-        print(f"Status of instance id {(i - 1)}: '{rsp["status"]}'")
-        quit()
-print("No running instances found.")
+    return rsp["status"]
+
+if len(sys.argv) > 1:
+    # If id is provided do the specific status call
+    status = getStatusForId(sys.argv[1])
+    print(f"Status of instance id {sys.argv[1]}: '{status}'")
+else:
+    # If no id provided, then find the highest non-idle id # and return its status
+    ts.sendMessage("{\"command\": \"SERVER_STATUS\"}")
+    capacity = ts.getResult()
+    jResult = json.loads(capacity)
+    for i in range(jResult["total"], 0, -1):
+        status = getStatusForId(str(i - 1))
+        if status != "IDLE":
+            print(f"Status of instance id {(i - 1)}: '{status}'")
+            quit()
+    print("No running instances found.")
